f8b0778ab8b0c3645012ebaf715fe20c3461c40d
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-05-18 00:07:22 -0600

    Primer commit del proyecto

---
+++ src/main/java/com/sistema/notificaciones/backend/SistemaNotificacionesApplication.java
@@ -0,0 +1,14 @@
+package com.sistema.notificaciones.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SistemaNotificacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaNotificacionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistema/notificaciones/backend/sistemanotificaciones/SistemaNotificacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.notificaciones.backend.sistemanotificaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaNotificacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

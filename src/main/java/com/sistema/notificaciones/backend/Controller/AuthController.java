30da18bdc3dd2e1c9bbc7119ca92fb13740906f9
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-05-22 23:56:24 -0600

    Se modificaron los endpoints para poder realizar el primer registro del usuario y realizar el login

--- src/main/java/com/sistema/notificaciones/backend/Controller/AuthController.java
+++ src/main/java/com/sistema/notificaciones/backend/Controller/AuthController.java
@@ -1,6 +1,13 @@
 package com.sistema.notificaciones.backend.Controller;
 
+import com.sistema.notificaciones.backend.Protegidos.Request.RegistroRequest;
+import com.sistema.notificaciones.backend.Request.LoginRequest;
+import com.sistema.notificaciones.backend.Response.AuthResponse;
+import com.sistema.notificaciones.backend.Service.AuthService;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +18,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthService authService;
+
     @PostMapping(value="login")
-    public String login(){
-        return "Esto es un login";
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request){
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @PostMapping(value="start-register")
+    public ResponseEntity<AuthResponse> startRegister(@RequestBody RegistroRequest request) {        
+        return ResponseEntity.ok(authService.register(request));
     }
 
 }

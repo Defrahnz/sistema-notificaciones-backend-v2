f70d91c1c146f7fc464d87250c18aadf4948296e
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-05-22 23:59:24 -0600

    Se creó la request para conectar los datos con la BD

---
+++ src/main/java/com/sistema/notificaciones/backend/Protegidos/Request/RegistroRequest.java
@@ -0,0 +1,26 @@
+package com.sistema.notificaciones.backend.Protegidos.Request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegistroRequest {
+    String nombre;
+    String apellidoP;
+    String apellidoM;
+    String calle;
+    String numInt;
+    String numExt;
+    String codigoPostal;
+    String colonia;
+    String ciudad;
+    String telefonoCasa;
+    String telefonoMovil;
+    String username;
+    String password;
+}

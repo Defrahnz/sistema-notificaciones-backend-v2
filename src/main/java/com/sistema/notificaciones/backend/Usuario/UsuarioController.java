7884af19702ce3987062e930ca948d8dd899a993
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-06-13 21:19:29 -0600

    Se agregó un Controller para poder manejar las peticiones del modelo de Usuario

---
+++ src/main/java/com/sistema/notificaciones/backend/Usuario/UsuarioController.java
@@ -0,0 +1,35 @@
+package com.sistema.notificaciones.backend.Usuario;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping(value="/api/v2/usuario")
+@RequiredArgsConstructor
+public class UsuarioController {
+    private final UsuarioService usuarioService;
+
+    @GetMapping(value="{id}")
+    public ResponseEntity<UsuarioDTO> getUsuario(@PathVariable Integer id) {
+        UsuarioDTO usuarioDTO=usuarioService.getUsuario(id);
+        if(usuarioDTO==null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(usuarioDTO);
+    }
+    
+    @PutMapping()
+    public ResponseEntity<UsuarioResponse> updateUsuario(@RequestBody UsuarioRequest usuarioRequest) {
+        return ResponseEntity.ok(usuarioService.updateUsuario(usuarioRequest));
+    }
+
+}

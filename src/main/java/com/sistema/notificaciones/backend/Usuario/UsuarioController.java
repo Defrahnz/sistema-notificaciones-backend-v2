163d26cbbed1f5655ce4637db6746642b312f93c
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-06-14 01:16:13 -0600

    Se agregaron las politicas de CORS

--- src/main/java/com/sistema/notificaciones/backend/Usuario/UsuarioController.java
+++ src/main/java/com/sistema/notificaciones/backend/Usuario/UsuarioController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping(value="/api/v2/usuario")
 @RequiredArgsConstructor
+@CrossOrigin(origins={"http://localhost:4200"})
 public class UsuarioController {
     private final UsuarioService usuarioService;
 

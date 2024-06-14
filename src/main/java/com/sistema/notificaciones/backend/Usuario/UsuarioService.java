6233a1b1f1a82b43b0d49c2cf6dd05cdd0be0ef1
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-06-13 21:20:29 -0600

    Se creó un Services para poder crear los objetos necesarios para el manejo de la BD

---
+++ src/main/java/com/sistema/notificaciones/backend/Usuario/UsuarioService.java
@@ -0,0 +1,63 @@
+package com.sistema.notificaciones.backend.Usuario;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+    private final UsuarioRepository usuarioRepository; 
+
+    @Transactional
+    public UsuarioResponse updateUsuario(UsuarioRequest usuarioRequest) {
+        Usuario usuario = Usuario.builder()
+            .id(usuarioRequest.getId())
+            .nombre(usuarioRequest.getNombre())
+            .apellidop(usuarioRequest.getApellidop())
+            .apellidom(usuarioRequest.getApellidom())
+            .calle(usuarioRequest.getCalle())
+            .numinterior(usuarioRequest.getNuminterior())
+            .numexterior(usuarioRequest.getNumexterior())
+            .codigopostal(usuarioRequest.getCodigopostal())
+            .colonia(usuarioRequest.getColonia())
+            .ciudad(usuarioRequest.getCiudad())
+            .telefonocasa(usuarioRequest.getTelefonocasa())
+            .telefonomovil(usuarioRequest.getTelefonomovil())
+            .username(usuarioRequest.getUsername())
+            .rol(Rol.USER) // Ajustar según el rol necesario
+            .build();
+        
+        usuarioRepository.updateUsuario(
+            usuario.getId(), usuario.getNombre(), usuario.getApellidop(), usuario.getApellidom(),
+            usuario.getCalle(), usuario.getNuminterior(), usuario.getNumexterior(), usuario.getCodigopostal(),
+            usuario.getColonia(), usuario.getCiudad(), usuario.getTelefonocasa(), usuario.getTelefonomovil(), usuario.getUsername()
+        );
+
+        return new UsuarioResponse("El usuario se actualizó satisfactoriamente");
+    }
+
+    public UsuarioDTO getUsuario(Integer id) {
+        Usuario usuario = usuarioRepository.findById(id).orElse(null);
+        if (usuario != null) {
+            UsuarioDTO usuarioDTO = UsuarioDTO.builder()
+                .id(usuario.getId())
+                .nombre(usuario.getNombre())
+                .apellidop(usuario.getApellidop())
+                .apellidom(usuario.getApellidom())
+                .calle(usuario.getCalle())
+                .numinterior(usuario.getNuminterior())
+                .numexterior(usuario.getNumexterior())
+                .codigopostal(usuario.getCodigopostal())
+                .colonia(usuario.getColonia())
+                .ciudad(usuario.getCiudad())
+                .telefonocasa(usuario.getTelefonocasa())
+                .telefonomovil(usuario.getTelefonomovil())
+                .username(usuario.getUsername())
+                .build();
+            return usuarioDTO;
+        }
+        return null;
+    }
+}

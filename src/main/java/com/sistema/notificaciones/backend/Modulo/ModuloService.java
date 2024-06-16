2e5b7fc7e54c3d9113500d3aac88263cdc47e2c4
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-06-16 00:17:50 -0600

    Reimaginamos el ModuloService agregando el patron de diseño Builder

--- src/main/java/com/sistema/notificaciones/backend/Modulo/ModuloService.java
+++ src/main/java/com/sistema/notificaciones/backend/Modulo/ModuloService.java
@@ -1,10 +1,29 @@
 package com.sistema.notificaciones.backend.Modulo;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
 
 
-public interface ModuloService {
+import lombok.RequiredArgsConstructor;
 
-    List<Modulo> obtenerModulosActivos();
+@Service
+@RequiredArgsConstructor
+public class ModuloService{
+    
+    private final ModuloRepository moduloRepository;
+    
+    public List<ModuloDTO> obtenerModulosActivos() {
+        List<Modulo> modulos = moduloRepository.findByActivoTrue();
+        return modulos.stream()
+                .map(modulo -> ModuloDTO.builder()
+                        .idModulo(modulo.getIdModulo())
+                        .nombreModulo(modulo.getNombreModulo())
+                        .urlImagen(modulo.getUrlImagen())
+                        .route(modulo.getUrl())  // Ajusta la ruta según tus necesidades
+                        .build())
+                .collect(Collectors.toList());
+    }
 
 }
